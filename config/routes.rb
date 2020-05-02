e105af511045449eb2503c432fa3933c9d4ee42c
mkmy1123 <mkmy1123@gmail.com>
2020-05-02 11:48:40 +0000

    Start Routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+  	root 'admin/home#top'
+  	resources :items, only:[:new, :create, :index, :show, :edit, :update]
+  	resources :customers, only:[:index, :show, :edit, :update]
+	end
+
+
+	scope '/' do
+	 		root 'customer/home#top'
+	end
+
 end
