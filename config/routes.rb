347cd2e42eb8b792b87eff4b8ff3b27a49461cd7
RyoTanaka0531 <vagrant@localhost.localdomain>
2020-05-02 12:18:57 +0000

    create genre order_item cart_item routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,19 @@ Rails.application.routes.draw do
   namespace :admin do
   	root 'admin/home#top'
   	resources :items, only:[:new, :create, :index, :show, :edit, :update]
-  	resources :customers, only:[:index, :show, :edit, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :order_items, only: [:update]
+    
 	end
 
+  get  'items' => 'customer/items#index', as: "customer_items"
+  get  'items/:id' => 'customer/items#show', as: "customer_item"
+  get 'cart_items' => 'customer/cart_items#index'
+  post 'cart_items' => 'customer/cart_items#create'
+  patch 'cart_items/:id' => 'customer/cart_items#update'
+  delete 'cart_items/:id' => 'customer/cart_items#destroy'
+  delete 'cart_items' => 'customer/cart_items#destroy_all'
 
 	scope '/' do
 	 		root 'customer/home#top'
