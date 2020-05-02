ea3162d4cbe507fc8162e4e7f4cf6c5f88b3cb50
MikiTatsuya199699 <tatsu.mikisan@gmail.com>
2020-05-02 05:16:37 +0000

    setting-default-in-ordertable-payment_method+order_status_and-notnull

--- db/migrate/20200502040209_create_orders.rb
+++ db/migrate/20200502040209_create_orders.rb
@@ -1,13 +1,13 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.integer :customer_id
-      t.string :postcode
-      t.string :address
-      t.integer :payment_method, default: 0
-      t.integer :order_status, default: 0
-      t.integer :postage
-      t.integer :payment
+      t.integer :customer_id, null:false
+      t.string :postcode, null:false
+      t.string :address, null:false
+      t.integer :payment_method, null:false, default: 0
+      t.integer :order_status, null:false, default: 0
+      t.integer :postage, null:false
+      t.integer :payment, null:false
 
       t.timestamps
     end
