8024c0fc1e0fd1d774921ed8b12bf39bfef2d82a
mkmy1123 <mkmy1123@gmail.com>
2020-05-02 12:16:34 +0000

    Routing Devise admin/customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+
+	devise_for :admins, skip: :all
+  devise_scope :admin do
+    get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
+    post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
+    delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'
+  end
+  devise_for :customers, skip: :all
+  devise_scope :customer do
+    get 'customers/sign_in' => 'customers/sessions#new', as: 'new_customer_session'
+    post 'customers/sign_in' => 'customers/sessions#create', as: 'customer_session'
+    delete 'customers/sign_out' => 'customers/sessions#destroy', as: 'destroy_customer_session'
+    get 'customers/sign_up' => 'customers/registrations#new', as: 'new_customer_registration'
+    post 'customers' => 'customers/registrations#create', as: 'customer_registration'
+    get 'customers/password/new' => 'customers/passwords#new', as: 'new_customer_password'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
@@ -12,6 +26,12 @@ Rails.application.routes.draw do
 
 	scope '/' do
 	 		root 'customer/home#top'
+	 		resources :customers, only:[:show, :edit, :update] do
+	 			member do
+    			get 'quit'
+    		end
+  		end
+  		resources :mailing_addresses, only:[:index, :create, :edit, :update, :destroy]
 	end
 
 end
