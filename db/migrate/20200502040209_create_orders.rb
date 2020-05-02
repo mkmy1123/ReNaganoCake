330af71d67bcf084b42e54e4e6894c9183970659
MikiTatsuya199699 <tatsu.mikisan@gmail.com>
2020-05-02 04:05:37 +0000

    order

---
+++ db/migrate/20200502040209_create_orders.rb
@@ -0,0 +1,15 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id
+      t.string :postcode
+      t.strig :address
+      t.integer :payment_method
+      t.integer :order_status
+      t.integer :postage
+      t.integer :payment
+
+      t.timestamps
+    end
+  end
+end
