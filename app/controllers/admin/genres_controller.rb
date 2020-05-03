e9174e2735f13b9bdc9121e8a11634a5cabf27d3
mkmy1123 <mkmy1123@gmail.com>
2020-05-03 09:26:33 +0000

    Improve Admin/items and more

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,22 @@
+class Admin::GenresController < ApplicationController
+    def create
+      @genre = Genre.new(genre_params)
+      @genre.save
+    end
+
+    def index
+      @genre = Genre.new
+      @genres = Genre.all
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+    def genre_params
+    	params.require(:genre).permit(:name,:is_valid)
+    end
+end
