44911b7b7c02a24879b4829405b79b9a28cc2365
mkmy1123 <mkmy1123@gmail.com>
2020-05-03 13:08:51 +0000

    Improve after signout path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,13 +11,12 @@ class ApplicationController < ActionController::Base
         end
     end
 
-    def after_sign_out_path_for(resource)
-        case resource
-        when Admin
-            new_admin_session_path
-        when Customer
-            root_path
-        end
+def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      new_customer_session_path
     end
+  end
 
 end
