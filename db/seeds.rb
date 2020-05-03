b9b70831a02bcd3c1bc3551dec76371447d137c7
RyoTanaka0531 <vagrant@localhost.localdomain>
2020-05-03 07:22:15 +0000

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+    email: 'test@test.com',
+    password: 'testtest'
+    )
+
+Customer.create!(
+    first_name: 'test',
+    last_name: 'customer',
+    kana_first_name: 'テスト',
+    kana_last_name: 'カスタマー',
+    postcode: 1234567,
+    address: '東京都品川区',
+    email: "customer@test.com",
+    password:              "customer",
+    password_confirmation: "customer")
