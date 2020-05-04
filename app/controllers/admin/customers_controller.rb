ad512880d6740df912b467be8c981b5825df6a92
RyoTanaka0531 <vagrant@localhost.localdomain>
2020-05-04 05:37:55 +0000

    update customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,16 +1,26 @@
 class Admin::CustomersController < ApplicationController
 
 	def index
+		@customers = Customer.all
 	end
 
 	def show
+		@customer = Customer.find(params[:id])
 	end
 
 	def edit
+		@customer = Customer.find(params[:id])
 	end
 
 	def update
+		@customer = Customer.find(params[:id])
+		@customer.update(customer_params)
+		redirect_to admin_customer_path(@customer)
 	end
 
+	private
+	def customer_params
+		params.require(:customer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :email, :postcode, :address)
+	end
 
 end
