3d9d65d69380876fda908c096a5bbf9adf069926
MikiTatsuya199699 <tatsu.mikisan@gmail.com>
2020-05-04 05:41:07 +0000

    create/admin/items/adit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -17,13 +17,20 @@ class Admin::ItemsController < ApplicationController
     end
 
     def edit
+      @item = find_item_by_id
     end
 
     def update
+      @item = find_item_by_id
+      @item.update(item_params)
+      redirect_to admin_item_path
     end
 
     private
     def item_params
         params.require(:item).permit(:genre_id, :name, :description, :price, :item_image, :is_selling)
+    end
+    def find_item_by_id
+        Item.find(params[:id])
     end
 end
