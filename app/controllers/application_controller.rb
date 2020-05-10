b3d72cdaf595361d19e1e9824cfc171f4eabcaeb
RyoTanaka0531 <vagrant@localhost.localdomain>
2020-05-10 04:56:42 +0000

    flashmessage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   # ログイン後のリダイレクト先
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(Admin)
-      admin_path 
+      admin_path
     else
-      root_path 
+      root_path
     end
   end
 
