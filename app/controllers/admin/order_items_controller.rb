b1fc5f5054085345e7135b829f701e3cf0c1ead9
RyoTanaka0531 <vagrant@localhost.localdomain>
2020-05-12 06:12:23 +0000

    update status

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -3,8 +3,15 @@ before_action :authenticate_admin!
 
 	def update
 		order_item = OrderItem.find(params[:id])
-		order_item.update(order_item_params)
-		redirect_to request.referrer
+		@order = order_item.order
+		# order = order.order_items.find(params[:id])
+		if params[:order_item][:create_status] == "製作中"
+			@order.update!(order_status: 2)
+		elsif params[:order_item][:create_status] == "製作完了"
+			@order.update!(order_status: 3)
+		end
+			order_item.update(order_item_params)
+			redirect_to request.referrer
 	end
 
 	private
