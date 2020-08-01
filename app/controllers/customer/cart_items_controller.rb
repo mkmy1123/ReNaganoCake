a1a26c3707646ba354ae5e5732e113a75fa1e871
mkmy1123 <mkmy1123@gmail.com>
2020-08-01 02:58:53 +0000

    IMPROVE basis syntax

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -9,28 +9,26 @@ class Customer::CartItemsController < ApplicationController
 	def destroy
 		cart_item = CartItem.find(params[:id])
 		cart_item.destroy
-		redirect_to '/cart_items'
+		redirect_to cart_items_path
 	end
 
 	def update
 		cart_item = CartItem.find(params[:id])
 		if cart_item.update(cart_params)
-			redirect_to '/cart_items'
+			redirect_to cart_items_path
 		end
 	end
 
 	def destroy_all
 		cart_items = CartItem.where(customer_id:[current_customer.id])
-		cart_items.each do |cart_item|
-			cart_item.destroy
-		end
-		redirect_to '/cart_items'
+		cart_items.destroy_all
+		redirect_to cart_items_path
 	end
 
 	def create
 		cart_item = CartItem.new(cart_params)
 		if cart_item.save
-			redirect_to '/cart_items'
+			redirect_to cart_items_path
 		else
 			flash[:notice] = "個数を選択してください"
 			redirect_to request.referrer
--- app/controllers/customer/home_controller.rb
+++ app/controllers/customer/home_controller.rb
@@ -1,13 +1,11 @@
 class Customer::HomeController < ApplicationController
 
     def top
-    	items = Item.where(is_selling: true)
+      items = Item.where(is_selling: true)
       @recomend_items = items.limit(9).order(updated_at: "DESC")
       if customer_signed_in?
         @cart_items = CartItem.where(customer_id:[current_customer.id])
       end
     end
 
-    def about
-    end
 end
